6b20b492dd26e31ab0d884e6798a41d32c3b59d1
PavelMalta <shamashov87@mail.ru>
2021-07-16 13:28:14 +0300

    added styles img

--- src/ui/components/imagePage/imageContainer/ImageContainer.tsx
+++ src/ui/components/imagePage/imageContainer/ImageContainer.tsx
@@ -1,25 +1,26 @@
-import { Grid } from '@material-ui/core';
+import {Grid} from '@material-ui/core';
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { AppRootStateType } from '../../../../bll/store';
-import { PhotoType } from '../../../../dal/api';
-import { ImageItem } from './imageItem/ImageItem';
+import {useSelector} from 'react-redux';
+import {AppRootStateType} from '../../../../bll/store';
+import {PhotoType} from '../../../../dal/api';
+import {ImageItem} from './imageItem/ImageItem';
 
 export const ImageContainer = () => {
-    
+
     //HOOK
     const photoData = useSelector<AppRootStateType, Array<PhotoType>>(state => state.image.imageData.photo)
-    
-    
+
+
     return (
-        <Grid container spacing={5}>
-            {photoData.map( item => {
-               return ( <ImageItem key={item.id}
-                           owner={item.owner}
-                           secret={item.secret}
-                           server={item.server}
-                           farm={item.farm}
-                           title={item.title}
+        <Grid container spacing={5} style={{justifyContent: "center"}}>
+            {photoData.map(item => {
+                return (<ImageItem key={item.id}
+                                   id={item.id}
+                                   owner={item.owner}
+                                   secret={item.secret}
+                                   server={item.server}
+                                   farm={item.farm}
+                                   title={item.title}
                 />)
             })}
         </Grid>
--- src/ui/components/imagePage/imageContainer/imageItem/ImageItem.tsx
+++ src/ui/components/imagePage/imageContainer/imageItem/ImageItem.tsx
@@ -1,7 +1,9 @@
-import { Grid, Paper } from '@material-ui/core';
+import {Grid, Paper} from '@material-ui/core';
 import React from 'react';
+import s from './ImageItem.module.css'
 
 type ImageItemPropsType = {
+    id: string
     owner: string
     secret: string
     server: string
@@ -10,13 +12,17 @@ type ImageItemPropsType = {
 }
 
 export const ImageItem: React.FC<ImageItemPropsType> = (
-    {owner, secret, server, farm, title}
-   ) => {
-    
+    {id, owner, secret, server, farm, title}
+) => {
+
+    const picUrl = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`
+
     return (
         <Grid item>
             <Paper elevation={3} style={{padding: "10px"}}>
-                <div>{title}</div>
+                <div className={s.photoBox}>
+                    <img src={picUrl} width="200px" height="200px"/>
+                </div>
             </Paper>
         </Grid>
     )
